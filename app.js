2caabe7193a408b13e4f32eb6116da8bbc6f317f
DavidOHanlon85 <159072612+DavidOHanlon85@users.noreply.github.com>
2024-12-16 22:10:38 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const PORT = 3000;
+
+const wishlist = [
+    { "item": "Playstation 5" },
+    { "item": "Some Science Fiction Novels"},
+    { "item": "Cadbury's Selection Box"},
+    { "item": "Super Slinky Guitar Strings"}
+]
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(morgan('tiny'));
+app.use(express.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index', { data: wishlist });
+});
+
+app.post('/', (req, res) => {
+    const newitem = req.body;
+    wishlist.push({"item": newitem.myitem});
+    res.render('index', { data: wishlist });
+});
+
+app.listen(PORT, (err) => {
+    if (err)
+        return console.log(err);
+
+    console.log(`Express listening on port ${PORT}`);
+});
